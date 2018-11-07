a424c0c56bc0f9c29a4aa8a52516fc123bebf11b
Javier Alvarez <javier.alvarez@Javiers-MacBook-Pro.local>
2018-11-07 18:20:17 -0500

    refactor(bestBuyApi): add param to showLoadingScreen

--- jssrc/iphone/require/bestBuyApi.js
+++ jssrc/iphone/require/bestBuyApi.js
@@ -1,3 +1,4 @@
+/* jshint esnext: true */
 define(() => {
     let bestBuyApi_ = class bestBuyApi {
         constructor() {
@@ -7,19 +8,19 @@ define(() => {
         }
         getCategories(operationSuccess, operationFailure, params) {
             let operationName = 'getCategories';
-            showLoadingScreen();
+            this.showLoadingScreen('Loading categories');
             return this.integrationObj.invokeOperation(operationName, {}, params, operationSuccess.bind(this), operationFailure.bind(this));
         }
         getProductsByCategory(operationSuccess, operationFailure, params) {
             let operationName = 'getProductsByCategory';
-            showLoadingScreen();
+            this.showLoadingScreen('Loading products');
             return this.integrationObj.invokeOperation(operationName, {}, params, operationSuccess.bind(this), operationFailure.bind(this));
         }
-        showLoadingScreen() {
-            kony.application.showLoadingScreen("skProgress", 'Loading categories', constants.LOADING_SCREEN_POSITION_ONLY_CENTER, true, true, null);
+        showLoadingScreen(description) {
+            kony.application.showLoadingScreen("skProgress", description, constants.LOADING_SCREEN_POSITION_ONLY_CENTER, true, true, null);
         }
-    }
+    };
     return {
         bestBuyApi: bestBuyApi_
-    }
+    };
 });
